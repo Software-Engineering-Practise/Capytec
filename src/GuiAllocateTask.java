2e525b8e63495cfe7ad97506241e1a0d4f886e9e
Kieran Robinson <robinson.kieran@outlook.com>
2021-05-21 17:44:20 +0100

    Dynamic task names and recommended skills for Allocate Task GUI
    Also added template code for dynamic user skills once login system is implemented

--- src/GuiAllocateTask.java
+++ src/GuiAllocateTask.java
@@ -22,11 +22,11 @@ import javax.swing.DefaultComboBoxModel;
 import java.awt.Color;
 import javax.swing.JCheckBox;
 import javax.swing.JTextPane;
+import javax.swing.JTextArea;
 
 public class GuiAllocateTask extends JFrame {
 
 	private JPanel contentPane;
-	private JTextField textFieldDescription;
 	CapyTecDB dbClass = new CapyTecDB();
 	/**
 	 * Launch the application.
@@ -55,21 +55,16 @@ public class GuiAllocateTask extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		textFieldDescription = new JTextField();
-		textFieldDescription.setColumns(10);
-		textFieldDescription.setBounds(191, 101, 172, 39);
-		contentPane.add(textFieldDescription);
-		
 		JLabel lblTaskName = new JLabel("Task Name:");
-		lblTaskName.setBounds(90, 70, 76, 14);
+		lblTaskName.setBounds(22, 70, 144, 14);
 		contentPane.add(lblTaskName);
 		
 		JLabel lblDescription = new JLabel("Description:");
-		lblDescription.setBounds(90, 101, 76, 14);
+		lblDescription.setBounds(22, 101, 144, 14);
 		contentPane.add(lblDescription);
 		
-		JLabel lblTaskType = new JLabel("Type:");
-		lblTaskType.setBounds(90, 155, 76, 14);
+		JLabel lblTaskType = new JLabel("Recommended Skills:");
+		lblTaskType.setBounds(22, 160, 144, 14);
 		contentPane.add(lblTaskType);
 		
 		JLabel lblAllocateTask = new JLabel("Allocate Task");
@@ -77,15 +72,7 @@ public class GuiAllocateTask extends JFrame {
 		lblAllocateTask.setBounds(87, 20, 202, 34);
 		contentPane.add(lblAllocateTask);
 		
-		for(int i=0; i<dbClass.getAllSkills().size(); i++) {
-			String currentSkill = dbClass.getAllSkills().get(i);
-			comboBoxTaskTypeOne.addItem(currentSkill);
-			comboBoxTaskTypeTwo.addItem(currentSkill);
-			comboBoxTaskTypeThree.addItem(currentSkill);
-			
-		}
-		
-		JLabel lblFrequency = new JLabel("Frequency(Days Until Repeat):");
+		JLabel lblFrequency = new JLabel("Days Until Repeat");
 		lblFrequency.setBounds(22, 283, 162, 14);
 		contentPane.add(lblFrequency);
 		
@@ -98,7 +85,7 @@ public class GuiAllocateTask extends JFrame {
 		JDatePanelImpl datePanelStart = new JDatePanelImpl(modelStartDate, pStart);
 		
 		JLabel lblStartDate = new JLabel("Start Date:");
-		lblStartDate.setBounds(90, 340, 76, 14);
+		lblStartDate.setBounds(22, 340, 144, 14);
 		contentPane.add(lblStartDate);
 		
 		UtilDateModel modelDueDate = new UtilDateModel();
@@ -109,19 +96,19 @@ public class GuiAllocateTask extends JFrame {
 		JDatePanelImpl datePanelDue = new JDatePanelImpl(modelDueDate, pDue);
 		
 		JLabel lblDueDate = new JLabel("Due Date:");
-		lblDueDate.setBounds(90, 380, 76, 14);
+		lblDueDate.setBounds(22, 380, 144, 14);
 		contentPane.add(lblDueDate);
 		
 		JLabel lblImportance = new JLabel("Importance:");
-		lblImportance.setBounds(90, 428, 76, 14);
+		lblImportance.setBounds(22, 428, 144, 14);
 		contentPane.add(lblImportance);
 		
 		JLabel lblNeedsSigning = new JLabel("Needs Signing:");
-		lblNeedsSigning.setBounds(90, 470, 135, 14);
+		lblNeedsSigning.setBounds(22, 470, 162, 14);
 		contentPane.add(lblNeedsSigning);
 		
 		JLabel lblNeedsPeerChecking = new JLabel("Needs Peer Checking:");
-		lblNeedsPeerChecking.setBounds(60, 500, 124, 14);
+		lblNeedsPeerChecking.setBounds(22, 500, 162, 14);
 		contentPane.add(lblNeedsPeerChecking);
 		
 		JLabel lblErrorMessage = new JLabel("Error Message");
@@ -130,171 +117,51 @@ public class GuiAllocateTask extends JFrame {
 		contentPane.add(lblErrorMessage);
 		lblErrorMessage.setVisible(false);
 		
+		JTextArea textAreaDescription = new JTextArea();
+		textAreaDescription.setBounds(95, 96, 317, 53);
+		//Add line wrapping after words in text field.
+		textAreaDescription.setLineWrap(true);
+		textAreaDescription.setWrapStyleWord(true);
+		contentPane.add(textAreaDescription);
 		
-		JButton btnInsertTask = new JButton("Insert Task");
-		btnInsertTask.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {				
-				String taskName = textFieldTaskName.getText();
-				String description = textFieldDescription.getText();
-				int frequency;
-				String taskTypeOne = comboBoxTaskTypeOne.getSelectedItem().toString();
-				String taskTypeTwo = comboBoxTaskTypeTwo.getSelectedItem().toString();
-				String taskTypeThree = comboBoxTaskTypeThree.getSelectedItem().toString();
-				String startDate = datePickerStart.getJFormattedTextField().getText();
-				String dueDate = datePickerDue.getJFormattedTextField().getText();
-				String importance = comboBoxImportance.getSelectedItem().toString();
-				int importanceNumeric = 0;
-				boolean needsSigning = chckbxNeedsSigning.isSelected();
-				boolean needsPeerChecking = chckbxNeedsPeerChecking.isSelected();
-				
-				switch(comboBoxFrequency.getSelectedItem().toString()){
-				case "1":
-					frequency = 1;
-					break;
-				case "7":
-					frequency = 7;
-					break;
-				case "14":
-					frequency = 14;
-					break;
-				case "30":
-					frequency = 30;
-					break;
-				case "182":
-					frequency = 182;
-					break;
-				case "365":
-					frequency = 365;
-					break;
-				default:
-					frequency = 0;
-				}
-
-				switch(importance) {
-				case "1":
-					importanceNumeric = 1;
-				case "2":
-					importanceNumeric = 2;
-				case "3":
-					importanceNumeric = 3;
-				}
-				boolean insertTaskError = false;
-				
-				if(dueDate.equals("")) {
-					//System.out.println("Due date cannot be empty");
-					lblErrorMessage.setText("Due date cannot be empty");
-					insertTaskError = true;
-				}
-				if(startDate.equals("")) {
-					//System.out.println("Start date cannot be empty");
-					lblErrorMessage.setText("Start date cannot be empty");
-					insertTaskError = true;
-				}
-				if(taskTypeOne.isEmpty()) {
-					//System.out.println("Task type cannot be empty");
-					lblErrorMessage.setText("First task type must be selected");
-					insertTaskError = true;
-				}
-				if(taskTypeOne.equals(taskTypeTwo) || taskTypeOne.equals(taskTypeThree)) {
-					lblErrorMessage.setText("Cannot have duplicate tasks");
-					insertTaskError = true;
-				}
-				if(taskTypeTwo.equals(taskTypeThree) && !taskTypeTwo.equals("")) {
-					lblErrorMessage.setText("Cannot have duplicate tasks");
-					insertTaskError = true;	
-				}
-				if(description.isBlank()) {
-					//System.out.println("Description cannot be empty");
-					lblErrorMessage.setText("Description cannot be empty");
-					insertTaskError = true;
-				}
-				if(taskName.isBlank()) {
-					//System.out.println("Task name cannot be empty");
-					lblErrorMessage.setText("Task name cannot be empty");
-					insertTaskError = true;
-				}
-				if(insertTaskError == false) {
-					System.out.println("Taskname: " + taskName);
-					System.out.println("Description: " + description);
-					System.out.println("Task Type One: " + taskTypeOne);
-					System.out.println("Task Type Two: " + taskTypeTwo);
-					System.out.println("Task Type Three: " + taskTypeThree);
-					System.out.println("Days Until Repeat: " + frequency);
-					System.out.println("Start Date: " + startDate);
-					System.out.println("Due Date: " + dueDate);
-					System.out.println("Needs Signing " + needsSigning);
-					System.out.println("Needs Peer Checking " + needsPeerChecking);
-					
-					lblErrorMessage.setVisible(false);
-					
-					//Insert task
-					CaretakerTask newTask = new CaretakerTask();
-					newTask.setTitle(taskName);
-					newTask.setDesc(description);
-					ArrayList<String> taskTypes = new ArrayList<String>();
-					taskTypes.add(taskTypeOne);
-					taskTypes.add(taskTypeTwo);
-					taskTypes.add(taskTypeThree);
-					newTask.setRecSkills(taskTypes);
-					newTask.setDaysUntilRepeat(frequency);
-					newTask.setDateCreated(startDate);
-					newTask.setDateDue(dueDate);
-					newTask.setPriority(importanceNumeric);
-					newTask.setNeedsSigning(needsSigning);
-					newTask.setNeedsPeerChecking(needsPeerChecking);
-					
-					//dbClass.addCaretakerTask(newTask);
-					//System.out.println("Insert");
-				} else {
-					lblErrorMessage.setVisible(true);
-				}
-			}
-		});
-		btnInsertTask.setBounds(294, 623, 118, 36);
-		contentPane.add(btnInsertTask);
-		
-		JComboBox comboBoxTaskName = new JComboBox();
-		comboBoxTaskName.setBounds(191, 68, 96, 22);
-		contentPane.add(comboBoxTaskName);
-		
-		JLabel lblTaskTypeOne = new JLabel("Task Type");
-		lblTaskTypeOne.setBounds(191, 155, 96, 14);
+		JLabel lblTaskTypeOne = new JLabel("Placeholder");
+		lblTaskTypeOne.setBounds(22, 183, 96, 14);
 		contentPane.add(lblTaskTypeOne);
 		
 		JLabel lblTaskTypeTwo = new JLabel("Placeholder");
-		lblTaskTypeTwo.setBounds(191, 182, 98, 14);
+		lblTaskTypeTwo.setBounds(22, 200, 98, 14);
 		contentPane.add(lblTaskTypeTwo);
 		
 		JLabel lblTaskTypeThree = new JLabel("Placeholder");
-		lblTaskTypeThree.setBounds(191, 208, 125, 14);
+		lblTaskTypeThree.setBounds(22, 219, 125, 14);
 		contentPane.add(lblTaskTypeThree);
 		
 		JLabel lblFrequencyValue = new JLabel("New label");
-		lblFrequencyValue.setBounds(191, 283, 46, 14);
+		lblFrequencyValue.setBounds(191, 283, 118, 14);
 		contentPane.add(lblFrequencyValue);
 		
 		JLabel lblStartDateValue = new JLabel("New label");
-		lblStartDateValue.setBounds(191, 340, 46, 14);
+		lblStartDateValue.setBounds(191, 340, 98, 14);
 		contentPane.add(lblStartDateValue);
 		
 		JLabel lblDueDateValue = new JLabel("New label");
-		lblDueDateValue.setBounds(191, 380, 46, 14);
+		lblDueDateValue.setBounds(191, 380, 98, 14);
 		contentPane.add(lblDueDateValue);
 		
 		JLabel lblImportanceValue = new JLabel("New label");
-		lblImportanceValue.setBounds(191, 428, 46, 14);
+		lblImportanceValue.setBounds(191, 428, 98, 14);
 		contentPane.add(lblImportanceValue);
 		
 		JLabel lblNeedsPeerCheckingValue = new JLabel("New label");
-		lblNeedsPeerCheckingValue.setBounds(191, 500, 46, 14);
+		lblNeedsPeerCheckingValue.setBounds(191, 500, 98, 14);
 		contentPane.add(lblNeedsPeerCheckingValue);
 		
 		JLabel lblNeedsSigningValue = new JLabel("New label");
-		lblNeedsSigningValue.setBounds(191, 470, 46, 14);
+		lblNeedsSigningValue.setBounds(191, 470, 98, 14);
 		contentPane.add(lblNeedsSigningValue);
 		
 		JLabel lblExtraConsiderations = new JLabel("Extra Considerations:");
-		lblExtraConsiderations.setBounds(60, 531, 112, 14);
+		lblExtraConsiderations.setBounds(22, 531, 150, 14);
 		contentPane.add(lblExtraConsiderations);
 		
 		JTextPane textPaneExtraConsiderations = new JTextPane();
@@ -302,20 +169,196 @@ public class GuiAllocateTask extends JFrame {
 		contentPane.add(textPaneExtraConsiderations);
 		
 		JLabel lblUserSkills = new JLabel("Your Skills:");
-		lblUserSkills.setBounds(317, 11, 83, 14);
+		lblUserSkills.setBounds(157, 160, 83, 14);
 		contentPane.add(lblUserSkills);
 		
-		JLabel lblUsersSkillOne = new JLabel("New label");
-		lblUsersSkillOne.setBounds(317, 34, 76, 14);
-		contentPane.add(lblUsersSkillOne);
+		JLabel lblUserSkillOne = new JLabel("New label");
+		lblUserSkillOne.setBounds(157, 183, 76, 14);
+		contentPane.add(lblUserSkillOne);
 		
-		JLabel lblUsersSkillOne_1 = new JLabel("New label");
-		lblUsersSkillOne_1.setBounds(317, 51, 76, 14);
-		contentPane.add(lblUsersSkillOne_1);
+		JLabel lblUserSkillTwo = new JLabel("New label");
+		lblUserSkillTwo.setBounds(157, 200, 76, 14);
+		contentPane.add(lblUserSkillTwo);
 		
-		JLabel lblUsersSkillOne_1_1 = new JLabel("New label");
-		lblUsersSkillOne_1_1.setBounds(317, 70, 76, 14);
-		contentPane.add(lblUsersSkillOne_1_1);
+		JLabel lblUserSkillThree = new JLabel("New label");
+		lblUserSkillThree.setBounds(157, 219, 76, 14);
+		contentPane.add(lblUserSkillThree);
+		
+		JButton btnAllocateTask = new JButton("Allocate Task");
+		
+		btnAllocateTask.setBounds(294, 623, 118, 36);
+		contentPane.add(btnAllocateTask);
+		
+		JComboBox<String> comboBoxTaskName = new JComboBox<String>();
+		
+		//Populate task name dropdown
+		for(int i=0; i<dbClass.getAllTasks().size(); i++) {
+			CaretakerTask currentTask = dbClass.getAllTasks().get(i);
+			String currentTaskName = currentTask.getTitle();	
+			comboBoxTaskName.addItem(currentTaskName);
+		}
+		
+		//Populate initial values
+		for(int i=0; i<dbClass.getAllTasks().size(); i++) {
+			CaretakerTask currentTask = dbClass.getAllTasks().get(i);
+			String currentTaskDesc = currentTask.getDesc();	
+			String recSkillOne = "";
+			String recSkillTwo = "";
+			String recSkillThree = "";
+			if(currentTask.getTitle().equals(comboBoxTaskName.getSelectedItem().toString())) {
+				ArrayList<String> recSkills = currentTask.getRecSkills();
+				int noOfRecSkills = recSkills.size();
+				switch(noOfRecSkills) {
+				case 1:
+					recSkillOne = recSkills.get(0);
+					break;
+				case 2:
+					recSkillOne = recSkills.get(0);
+					recSkillTwo = recSkills.get(1);
+					break;
+				case 3:
+					recSkillOne = recSkills.get(0);
+					recSkillTwo = recSkills.get(1);
+					recSkillThree = recSkills.get(2);
+					break;
+					
+				}
+				textAreaDescription.setText(currentTaskDesc);
+				lblTaskTypeOne.setText(recSkillOne);
+				lblTaskTypeTwo.setText(recSkillTwo);
+				lblTaskTypeThree.setText(recSkillThree);
+			}	
+		}
+		
+		//Update values on change of task name dropdown
+		comboBoxTaskName.addActionListener(new ActionListener() {
+			
+			public void actionPerformed(ActionEvent e) {
+				for(int i=0; i<dbClass.getAllTasks().size(); i++) {
+					CaretakerTask currentTask = dbClass.getAllTasks().get(i);
+					String currentTaskDesc = currentTask.getDesc();	
+					String recSkillOne = "";
+					String recSkillTwo = "";
+					String recSkillThree = "";
+					if(currentTask.getTitle().equals(comboBoxTaskName.getSelectedItem().toString())) {
+						ArrayList<String> recSkills = currentTask.getRecSkills();
+						int noOfRecSkills = recSkills.size();
+						switch(noOfRecSkills) {
+						case 1:
+							recSkillOne = recSkills.get(0);
+							break;
+						case 2:
+							recSkillOne = recSkills.get(0);
+							recSkillTwo = recSkills.get(1);
+							break;
+						case 3:
+							recSkillOne = recSkills.get(0);
+							recSkillTwo = recSkills.get(1);
+							recSkillThree = recSkills.get(2);
+							break;
+							
+						}
+						
+						/* Template code for when login system implemented
+						ArrayList<String> userSkills = loggedInUser.getSkills();
+						int noOfUserSkills = userSkills.size();
+						switch(noOfUserSkills) {
+						case 1:
+							userSkillOne = userSkills.get(0);
+							break;
+						case 2:
+							userSkillOne = userSkills.get(0);
+							userSkillTwo = userSkills.get(1);
+							break;
+						case 3:
+							userSkillOne = userSkills.get(0);
+							userSkillTwo = userSkills.get(1);
+							userSkillThree = userSkills.get(2);
+							break;
+						}
+						*/
+						
+						
+						
+						textAreaDescription.setText(currentTaskDesc);
+						lblTaskTypeOne.setText(recSkillOne);
+						lblTaskTypeTwo.setText(recSkillTwo);
+						lblTaskTypeThree.setText(recSkillThree);
+						//lblUserSkillOne.setText(userSkillOne);
+						//lblUserSkillTwo.setText(userSkillTwo);
+						//lblUserSkillThree.setText(userSkillThree);
+						
+					}	
+				}
+			}
+		});
+		
+		
+		
+		
+		
+		/*
+		comboBoxTaskName.addActionListener(new ActionListener() {
+			
+			public void actionPerformed(ActionEvent e) {
+				for(int i=0; i<dbClass.getAllTasks().size(); i++) {			
+					CaretakerTask currentTask = dbClass.getAllTasks().get(i);
+					if(currentTask.getTitle().equals(comboBoxTaskName.getSelectedItem().toString())) {
+						String recSkillOne = "Placeholder";
+						String recSkillTwo = ""; 
+						String recSkillThree = "";
+						ArrayList<String> recSkills = currentTask.getRecSkills();
+						int noOfRecSkills = recSkills.size();
+						switch(noOfRecSkills) {
+						case 1:
+							recSkillOne = recSkills.get(0);
+							break;
+						case 2:
+							recSkillOne = recSkills.get(0);
+							recSkillTwo = recSkills.get(1);
+							break;
+						case 3:
+							recSkillOne = recSkills.get(0);
+							recSkillTwo = recSkills.get(1);
+							recSkillThree = recSkills.get(2);
+							break;
+							
+					}
+						
+						
+						
+						
+						
+						
+						
+						
+						String currentTaskName = currentTask.getTitle();
+						String currentTaskDesc = currentTask.getDesc();
+						comboBoxTaskName.addItem(currentTaskName);
+						textAreaDescription.setText(currentTaskDesc);
+						lblTaskTypeOne.setText(recSkillOne);
+						lblTaskTypeTwo.setText(recSkillTwo);
+						lblTaskTypeThree.setText(recSkillThree);
+					}
+					
+					
+					
+					
+				}
+				
+				
+				//CaretakerTask currentTask = dbClass.getAllTasks().get(i);
+			}
+		});
+		*/
+		comboBoxTaskName.setBounds(97, 68, 266, 22);
+		contentPane.add(comboBoxTaskName);
+		
+		
+		
+		
+		
+		
 		
 		
 		
