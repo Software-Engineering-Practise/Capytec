6eaaf3d9644d74a9a1eb04dbb17399a5aa51ff49
Alex <alexanderdomheaton@gmail.com>
2021-05-23 16:18:07 +0100

    Finished Input user form

--- src/CapytecMain.java
+++ src/CapytecMain.java
@@ -13,7 +13,6 @@ public class CapytecMain {
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
-			//System.out.println("Not Logged In");
 		}
 		
 		boolean isManager = loginGui.checkUser(loginGui.getLoggedInId());
@@ -21,16 +20,6 @@ public class CapytecMain {
 		CapytecGui mainGui = new CapytecGui(isManager);
 		mainGui.setLoggedInId(loginGui.getLoggedInId());
 		mainGui.setVisible(true);
-		
-		
-		if(isManager) {
-			System.out.println("Manager Mode");
-			mainGui.setVisible(true);
-		}else {
-			mainGui.setVisible(true);
-			System.out.println("Caretaker Mode");
-		}
-		
 	}
 
 }
--- src/GuiInsertUser.java
+++ src/GuiInsertUser.java
@@ -1,3 +1,4 @@
+import java.awt.Color;
 import java.awt.EventQueue;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
@@ -7,6 +8,7 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JComboBox;
@@ -18,6 +20,12 @@ public class GuiInsertUser extends JFrame {
 	private JTextField textFieldFirstName;
 	private JTextField textFieldSurname;
 	CapyTecDB dbClass = new CapyTecDB();
+	private JTextField textFieldUsername;
+	private JTextField textconfirmPwdField;
+	private JPasswordField passwordField;
+	private JPasswordField passwordField_1;
+	private JPasswordField passwordFieldIn;
+	private JPasswordField passwordFieldConfirm;
 	/**
 	 * Launch the application.
 	 */
@@ -38,8 +46,8 @@ public class GuiInsertUser extends JFrame {
 	 * Create the frame.
 	 */
 	public GuiInsertUser() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		setBounds(100, 100, 449, 474);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
@@ -55,6 +63,11 @@ public class GuiInsertUser extends JFrame {
 		textFieldSurname.setBounds(191, 101, 96, 20);
 		contentPane.add(textFieldSurname);
 		
+		textFieldUsername = new JTextField();
+		textFieldUsername.setBounds(191, 132, 96, 20);
+		contentPane.add(textFieldUsername);
+		textFieldUsername.setColumns(10);
+		
 		JLabel lblFirstName = new JLabel("First Name:");
 		lblFirstName.setBounds(90, 70, 76, 14);
 		contentPane.add(lblFirstName);
@@ -64,7 +77,7 @@ public class GuiInsertUser extends JFrame {
 		contentPane.add(lblSurname);
 		
 		JLabel lblPosition = new JLabel("Position:");
-		lblPosition.setBounds(90, 132, 76, 14);
+		lblPosition.setBounds(90, 231, 76, 14);
 		contentPane.add(lblPosition);
 		
 		JLabel lblInsertUser = new JLabel("Insert User");
@@ -73,15 +86,97 @@ public class GuiInsertUser extends JFrame {
 		contentPane.add(lblInsertUser);
 		
 		JComboBox comboBoxPosition = new JComboBox();
+		comboBoxPosition.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
 		comboBoxPosition.setModel(new DefaultComboBoxModel(new String[] {"Caretaker", "Manager"}));
-		comboBoxPosition.setBounds(191, 131, 96, 22);
+		comboBoxPosition.setBounds(191, 227, 96, 22);
 		contentPane.add(comboBoxPosition);
+		
+		JComboBox<String> comboBoxSkill1 = new JComboBox();
+		comboBoxSkill1.setModel(new DefaultComboBoxModel<String>(new String[] {""}));
+		comboBoxSkill1.setBounds(191, 260, 96, 24);
+		contentPane.add(comboBoxSkill1);
+		
+		JComboBox<String> comboBoxSkill2 = new JComboBox();
+		comboBoxSkill2.setModel(new DefaultComboBoxModel<String>(new String[] {""}));
+		comboBoxSkill2.setBounds(191, 295, 96, 24);
+		contentPane.add(comboBoxSkill2);
+		
+		JComboBox<String> comboBoxSkill3 = new JComboBox();
+		comboBoxSkill3.setModel(new DefaultComboBoxModel<String>(new String[] {""}));
+		comboBoxSkill3.setBounds(191, 330, 96, 24);
+		contentPane.add(comboBoxSkill3);
+		
+		JLabel lblUsrBox = new JLabel("New label");
+		lblUsrBox.setForeground(new Color(204, 51, 0));
+		lblUsrBox.setBounds(297, 135, 126, 14);
+		contentPane.add(lblUsrBox);
+		
+		JLabel lblPwdBox = new JLabel("New label");
+		lblPwdBox.setForeground(new Color(204, 51, 0));
+		lblPwdBox.setBounds(297, 166, 126, 14);
+		contentPane.add(lblPwdBox);
+		
+		JLabel lblFName = new JLabel("New label");
+		lblFName.setForeground(new Color(204, 51, 0));
+		lblFName.setBounds(297, 73, 126, 14);
+		contentPane.add(lblFName);
+		
+		JLabel lblSName = new JLabel("New label");
+		lblSName.setForeground(new Color(204, 51, 0));
+		lblSName.setBounds(297, 104, 126, 14);
+		contentPane.add(lblSName);
+		
+		JLabel lblSkill1 = new JLabel("Skill 1:");
+		lblSkill1.setBounds(90, 265, 76, 14);
+		contentPane.add(lblSkill1);
+		
+		JLabel lblSkill2 = new JLabel("Skill 2:");
+		lblSkill2.setBounds(90, 300, 76, 14);
+		contentPane.add(lblSkill2);
+		
+		JLabel lblSkill3 = new JLabel("Skill 3:");
+		lblSkill3.setBounds(90, 335, 76, 14);
+		contentPane.add(lblSkill3);
+		
+		JLabel lblUsername = new JLabel("Username:");
+		lblUsername.setBounds(90, 135, 76, 14);
+		contentPane.add(lblUsername);
+		
+		JLabel lblPassword = new JLabel("Password:");
+		lblPassword.setBounds(90, 166, 60, 14);
+		contentPane.add(lblPassword);
+		
+		passwordFieldIn = new JPasswordField();
+		passwordFieldIn.setBounds(191, 163, 96, 20);
+		contentPane.add(passwordFieldIn);
+		
+		passwordFieldConfirm = new JPasswordField();
+		passwordFieldConfirm.setBounds(191, 194, 96, 20);
+		contentPane.add(passwordFieldConfirm);
+		
+		JLabel lblNewLabel = new JLabel("Confirm Password:");
+		lblNewLabel.setBounds(80, 197, 149, 14);
+		contentPane.add(lblNewLabel);
+		
+		
+		//Populate user skill comboboxes if caretaker is selected
+		for(int i=0; i<dbClass.getAllSkills().size(); i++) {
+			String currentSkill = dbClass.getAllSkills().get(i);
+			comboBoxSkill1.addItem(currentSkill);
+			comboBoxSkill2.addItem(currentSkill);
+			comboBoxSkill3.addItem(currentSkill);
+		}
+		
 		
 		JButton btnInsertUser = new JButton("Insert User");
 		btnInsertUser.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				String firstName = textFieldFirstName.getText();
 				String surname = textFieldSurname.getText();
+				String username = textFieldUsername.getText();
 				String position = comboBoxPosition.getSelectedItem().toString();
 				if(firstName.isBlank() || surname.isBlank()) {
 					System.out.println("Name cannot be empty");
@@ -100,8 +195,8 @@ public class GuiInsertUser extends JFrame {
 				
 			}
 		});
-		btnInsertUser.setBounds(304, 211, 118, 36);
+		btnInsertUser.setBounds(305, 388, 118, 36);
 		contentPane.add(btnInsertUser);
-
+		
 	}
 }
