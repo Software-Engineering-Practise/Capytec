8c89b514bff875c6fa3a5ec60c16dc71add7de6b
Alex <alexanderdomheaton@gmail.com>
2021-05-23 18:38:06 +0100

    Fixes and disabling of printlns

--- src/CapyTecDB.java
+++ src/CapyTecDB.java
@@ -755,4 +755,34 @@ public class CapyTecDB{
 		}
 		return id;
 	}
+	
+	public void addLogin(String hash, String username, int userId) {
+		
+		if(username != null) {
+			username = username.replaceAll("[^\\x00-\\x7F]", "");
+			username = username.replaceAll("[';']", "");
+		}
+		
+		String sql = "INSERT INTO login (login_user, login_username, login_pass) VALUES ("+userId+", '"+username+"', '"+hash+"');";
+		
+		database.RunSQL(sql);
+		
+	}
+	
+	public int getLastInsertId() {
+		
+		int id = 0;
+		String sql = "select last_insert_rowid();";
+		
+		ResultSet result = database.RunSQLQuery(sql);
+		
+		try {
+			id = result.getInt(1);
+		} catch (SQLException e) {
+			
+		}
+		
+		return id;
+	}
+	
 }
